f4d4b1c2474f6045441ad156d7bb91baadced8a2
melvin <m.bootsgezel@skybites.nl>
2015-03-19 14:18:19 +0100

    init

---
+++ src/Starter.java
@@ -0,0 +1,10 @@
+
+public class Starter {
+	
+	public static void main(String args[]){
+		
+		ThreadHandler th = ThreadHandler.getInstance();
+		
+	}
+
+}
---
+++ src/ThreadHandler.java
@@ -0,0 +1,22 @@
+
+public class ThreadHandler implements Runnable{
+	
+	private static ThreadHandler instance;
+	
+	private ThreadHandler() {
+		run();
+	}
+
+	@Override
+	public void run() {
+		new Thread(new Client("172.19.82.155", 25000)).start();
+	}
+	
+	public static ThreadHandler getInstance(){
+		if(instance == null){
+			instance = new ThreadHandler();
+		}
+		return instance;
+	}
+
+}
