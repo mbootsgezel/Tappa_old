696b26cee9159b8354d610d0691d3d65399b3bb5
melvin <m.bootsgezel@skybites.nl>
2015-03-24 18:12:06 +0100

    - Now includes miliseconds

--- src/server/CurrentDate.java
+++ src/server/CurrentDate.java
@@ -6,11 +6,11 @@ import java.util.Date;
 
 public class CurrentDate {
 	
-	private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
 	
 	public String now(){
 		Date now = new Date();
-		return sdf.format(now);
+		return sdf.format(new Date());
 	}
 
 }
